ec202391ac1936f210de078a478b52160fe03c05
Sam Reagan <samreagan@Sams-Laptop.local>
2025-10-22 04:59:01 -0500

    chore: update car details page, add some custom styles

--- ui-cars-app/src/app/layout.tsx
+++ ui-cars-app/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <header className="p-4 font-semibold">
+          <Link href="/">Car Management System</Link></header>
+        <main className="p-4">{children}</main>
+        {/* <footer>&copy; {new Date().getFullYear()}</footer> */}
       </body>
     </html>
   );
